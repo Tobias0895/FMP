f7df49203a75c9505b9c86ba6c24972ca78dfaa6
Tobias0895 <tobias1911@live.nl>
2023-11-10 13:51:25 +0100

    Integrating G into the luminosity function

--- Create_lambda.py
+++ Create_lambda.py
@@ -4,26 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from scipy.interpolate import interp1d
+
 # %%
 # Well try to create a lambda function fist for one wavelength
 import ChiantiPy.tools.filters as chfilters
 
-wvl = np.geomspace(0.1, 50, 5000) #angstrom
-temperature = np.logspace(4, 8, 100)
-
-density = 1.e+9
-emeasure = np.full_like(temperature, 1e27)
-s = ch.spectrum(temperature, density, wvl, filter = (chfilters.gaussian, 0.5), em = emeasure, doContinuum=1, minAbund=1.e-5, verbose=0)
+wvl = np.linspace(0.1, 180, 2001) #angstrom
+temperature = np.logspace(4, 8, 201)
+abund =1.e-4
+density = 1.e+10
+s = ch.spectrum(temperature, density, wvl, 
+                filter = (chfilters.gaussian, 0.1),
+                em = None,
+                doContinuum=True,
+                minAbund=abund, 
+                verbose=False)
 # %%
-import matplotlib as mpl
-L, T = np.meshgrid(wvl, temperature[60:])
+from matplotlib.colors import LogNorm
 
-plt.pcolormesh(L, T, s.Spectrum['intensity'][60:], shading='gouraud', norm='log')
+L, T = np.meshgrid(wvl, temperature)
+np.save('L.npy', L)
+np.save('T.npy', T)
+np.save(f'G-{abund}.npy', s.Spectrum['intensity'])
+vmax = np.max(s.Spectrum['intensity'])
+norm = LogNorm(vmax=vmax, vmin=vmax/1e6, clip=True)
+plt.pcolormesh(L, T, s.Spectrum['intensity'], shading='gouraud', norm=norm, cmap='plasma')
 plt.yscale('log')
 plt.ylabel('log(T [K])')
 plt.xlabel(s.Spectrum['xlabel'])
 plt.savefig('Figures/2D_G function', dpi=500)
-plt.colorbar()
+plt.colorbar(extend='min', label=s.Spectrum['ylabel'])
 plt.show()
 # %%
 
--- Test_site.py
+++ Test_site.py
@@ -4,8 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from scipy.interpolate import NearestNDInterpolator
+# import numba
 
 # %%
+# @jit(nonpython=True)
 # Import the data
 data_loc = os.environ['FMPdata']
 ds = Dataset.from_file(data_loc + '/3d__var_3_n00060000.plt')
@@ -16,12 +18,28 @@ ds_points = np.stack([ds(name) for name in ds.variables[:3]], axis=-1)
 ds_data = np.stack([ds(name) for name in ds.variables], axis=-1)
 interpolater = NearestNDInterpolator(ds_points, ds_data)
 # %%
-def G(T):
-    mask = (T <= 5e4) + (T >= 1e5)
-    G_temps = np.where(mask==0, T, 0)
+def find_nearest(array, value):
+    array = np.asarray(array)
+    lowest_idx = abs(array - value).argmin()
+    return lowest_idx
+
+
+def G(T, wavelength_band:tuple):
+    G_total = np.load('G-5e-05.npy')
+    wvl_array = np.load('L.npy')[0]
+    T_array = np.load('T.npy')[0]
+    min_wvl_idx = find_nearest(wvl_array, wavelength_band[0])
+    max_wvl_idx = find_nearest(wvl_array, wavelength_band[1])
+    G_band = G_total[...,min_wvl_idx:max_wvl_idx]
+
+
+
+
+    mask = T > 1e6
+    G_temps = np.where(mask==0, T, 0) # where the mask is False keep T, else 0
     return G_temps
 
-def integrate_along_line_of_sight(direction, stellar_radius, interpolater, image_radius=40, pixel_count=60):
+def integrate_along_line_of_sight(direction, stellar_radius, interpolater, image_radius=40, pixel_count=60, *args, **kwargs):
 
     # We first create a 3D meshgrid
     x = np.linspace(-1*image_radius, image_radius, pixel_count)
@@ -40,7 +58,7 @@ def integrate_along_line_of_sight(direction, stellar_radius, interpolater, image
     if car.lower() == 'x':
         mask_shadow = Y ** 2 + Z ** 2 <= stellar_radius ** 2
         if fb == "+":
-            mask_shadow *= (X < 0)
+           mask_shadow *= (X < 0)
         elif fb == '-':
             mask_shadow *= (X > 0)
     elif car.lower() == 'y':
@@ -60,7 +78,7 @@ def integrate_along_line_of_sight(direction, stellar_radius, interpolater, image
     mask = mask_star + mask_shadow
     density_minus_star = np.where(mask==0, interpolated_data[...,ds.variables.index('Rho [g/cm^3]')], 0)
     temperature_minus_star = np.where(mask==0, interpolated_data[...,ds.variables.index('te [K]')], 0)
-    integrand = np.square(density_minus_star) * G(temperature_minus_star)
+    integrand = np.square(density_minus_star) * G(temperature_minus_star, *args)
 
     # Now we integrate along the line of sight
     if car.lower() == "x":
@@ -74,11 +92,32 @@ def integrate_along_line_of_sight(direction, stellar_radius, interpolater, image
         return total_flux, (X[...,0], Y[...,0])
     else:
         raise ValueError
+    
+def total_lum_wvl_bin(wvl_bin:tuple, **kwargs) -> float:
+    LoS_flux, mesh = integrate_along_line_of_sight(wvl_bin, **kwargs)
+    Los_array = np.ravel(LoS_flux)
+    total_lux_in_band = np.trapz(Los_array, dx=np.diff(mesh[0])[0,0])
+    return total_lux_in_band
+
+
 
 # %%
 tobi, mesh = integrate_along_line_of_sight('+z', 1, interpolater, pixel_count=100)
+# print(mesh.shape)
 plt.pcolormesh(mesh[0], mesh[1], tobi, norm='log', shading='gouraud')
 plt.axis('equal')
 plt.colorbar()
 plt.savefig('Figures/intergrated_line_of_sight_x.png', dpi=500)
 plt.show()
+
+total_lum_wvl_bin((0,2), direction='+z', stellar_radius=1, interpolater=interpolater, pixel_count=50)
+# %%
+wavelength_band = (6,8)
+G_total = np.load('G-5e-05.npy')
+print(G_total.shape)
+wvl_array = np.load('L.npy')[0]
+T_array = np.load('T.npy')[0]
+a = find_nearest(wvl_array, wavelength_band[0])
+b = find_nearest(wvl_array, wavelength_band[1])
+G_total[...,a:b].shape
+
